2ddd395e12d5b3ccca6bb117f05547269b9d5dd6
Bishal Saha <bishalsaha2623@gmail.com>
2026-04-16 13:00:53 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: 'legiflow-p4ure',
   appId: '1:846977878595:web:e4f57e8065910d072e62f6',
   storageBucket: 'legiflow-p4ure.firebasestorage.app',
-  apiKey: 'AIzaSyAFK8XYbmIYiKyB5ylW4Tu4usDyxTxm08Y',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY
   authDomain: 'legiflow-p4ure.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '846977878595',
